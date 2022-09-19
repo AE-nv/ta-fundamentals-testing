3a2e71ce2507bf6ae7bac74394151080879dec7e
Arne Van de Bemdt <arne.vandebemdt@ae.be>
2022-09-19 19:27:51 +0200

    restructure project to add integration testing module

---
+++ integration-testing/src/test/java/be/ae/technicalqafundamentals/TechnicalQaFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ae.technicalqafundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalQaFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
