9044d144563aeb9e1764e139069a17c23bec0ae9
Arne Van de Bemdt <arne.vandebemdt@ae.be>
2022-09-19 22:05:46 +0200

    add testcontainers setup

--- integration-testing/src/test/java/be/ae/technicalqafundamentals/TechnicalQaFundamentalsApplicationTests.java
+++ integration-testing/src/test/java/be/ae/technicalqafundamentals/TechnicalQaFundamentalsApplicationTests.java
@@ -2,8 +2,10 @@ package be.ae.technicalqafundamentals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TechnicalQaFundamentalsApplicationTests {
 
 	@Test
