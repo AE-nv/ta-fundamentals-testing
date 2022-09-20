46ba5c8b7cb60b184c38fe46246e60783bab3b91
Arne Van de Bemdt <arne.vandebemdt@ae.be>
2022-09-20 15:50:46 +0200

    add integration testing setup

--- integration-testing/src/test/java/be/ae/technicalqafundamentals/TechnicalQaFundamentalsApplicationTests.java
+++ integration-testing/src/test/java/be/ae/technicalqafundamentals/TechnicalQaFundamentalsApplicationTests.java
@@ -1,15 +1,25 @@
 package be.ae.technicalqafundamentals;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class TechnicalQaFundamentalsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int localServerPort;
 
+    @BeforeEach
+    void setup() {
+        RestAssured.port = localServerPort;
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
