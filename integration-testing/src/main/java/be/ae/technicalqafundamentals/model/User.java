9f876238a5ef462a5f031ae863a608131a1e63d1
Arne Van de Bemdt <arne.vandebemdt@ae.be>
2022-09-20 08:43:53 +0200

    add specific GenerationType to id in User

--- integration-testing/src/main/java/be/ae/technicalqafundamentals/model/User.java
+++ integration-testing/src/main/java/be/ae/technicalqafundamentals/model/User.java
@@ -1,16 +1,13 @@
 package be.ae.technicalqafundamentals.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
